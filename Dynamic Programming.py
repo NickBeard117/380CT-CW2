16b44f003fbd61971cecbb8fcbffbd02f0e6ab9a
NickBeard117 <beardn@uni.coventry.ac.uk>
2016-12-30 16:20:39 +0000

    Update Dynamic Programming.py

--- Dynamic Programming.py
+++ Dynamic Programming.py
@@ -2,17 +2,26 @@ import random
 from random import randint
 import timeit
 
-array = sorted(random.sample(range(50), 25)) #create an array between 0 and *. CHANGE the first value to increase the range and the second value to increase the list size
-target = randint (0, 200) #target can be between * and the *
+n = 26 #length of array
+bitlength = 10 #specify bit length
+max_n_bit_number = 2**bitlength-1 #the max bit length is 2*bitlength-1
+array = sorted( [ randint(0,max_n_bit_number) for i in range(n) ])#create a random sorted array
+target = randint(0,n*max_n_bit_number) #create a random target
+
+
 print (array, target) #print the array and target
 
 start = timeit.default_timer() #start time
 def dynamic(array, i, target): #array, i for the location in the array, target
-    if i >= len(array):#if i is greater than the length of the array
-        if target == 0: #and the target is equal to 0 (it has been found)
-            return 1 #success
-        else: #if the target is not equal to 0 (it hasn't been found)
-            return 0 #fail
+    if target == 0:
+        return 1    
+    else:
+        if i >= len(array):
+            if target == 0:
+                return 1 #success
+            else: #if the target is not equal to 0 (it hasn't been found)
+                return 0 #fail
+
             
     iterations = dynamic(array, i + 1, target)
     iterations += dynamic(array, i + 1, target - array[i])
@@ -22,3 +31,4 @@ def dynamic(array, i, target): #array, i for the location in the array, target
 print("Number of iterations: ", dynamic(array, 0, target)) # call function and print total number of iterations, 0 is a fail, anything above 0 is a success
 stop = timeit.default_timer() #the stop time for the program
 print (stop - start) #total runtime
+
