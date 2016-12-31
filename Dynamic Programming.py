d8fc92ffe4c2fb9e8a5c78faeee84e7e146d3738
NickBeard117 <beardn@uni.coventry.ac.uk>
2016-12-31 18:56:18 +0000

    Update Dynamic Programming.py
    accidentally did recursive exhaustive search instead of dynamic
    I am not a smart man

--- Dynamic Programming.py
+++ Dynamic Programming.py
@@ -2,31 +2,50 @@ import random
 from random import randint, sample
 import timeit
 
-n = 26 #length of array
+n = 20
 bitlength = 10 #specify bit length
 max_n_bit_number = 2**bitlength-1 #the max bit length is 2*bitlength-1
 array = sorted( [ randint(0,max_n_bit_number) for i in range(n) ])#create a random sorted array
 target = randint(0,n*max_n_bit_number) #create a random target
 #target = sum(sample(array, randint(0,n)))
 
+count = 0 #checking how many times it loops
 print (array, target) #print the array and target
-
 start = timeit.default_timer() #start time
-def dynamic(array, i, target): #array, i for the location in the array, target
-    if target == 0:
-        return 1    
-    else:
-        if i >= len(array):
-            if target == 0:
-                return 1 #success
-            else: #if the target is not equal to 0 (it hasn't been found)
-                return 0 #fail
-            
-    iterations = dynamic(array, i + 1, target)
-    iterations += dynamic(array, i + 1, target - array[i])
-    return iterations #call the function again
 
-print("Number of iterations: ", dynamic(array, 0, target)) # call function and print total number of iterations, 0 is a fail, anything above 0 is a success
+def dynamic(array, n, target, count):
+
+    for i in range (n):
+        count += 1
+        
+        #we add array[i] to each element of the array and the result is inc_array
+        inc_array = [x+array[i] for x in array]
+        #concatenate array and inc_array
+        array = (array + inc_array)    
+    
+        #if an array element is greater than the target, remove it
+        j=0
+        while j <(len(array)):
+            if array[j] > target:
+                del array [j]
+            else:
+                j+= 1
+
+        
+        #check the array to see if the target has been found
+        for k in range (len(array)):
+            if array[k] == target or target == 0:
+                print ('true')
+                print ("Iterations: ",count)
+                return 1
+           
+    print ("False. Iterations: ",count)
+    
+
+
+dynamic(array, n, target, count)
+
 stop = timeit.default_timer() #the stop time for the program
 print (stop - start) #total runtime
+
 
