5c131fb840721d53725cd62a43ec8ea1ed96967f
NickBeard117 <beardn@uni.coventry.ac.uk>
2017-01-02 12:42:15 +0000

    added special cases

--- 380CT CW2.py
+++ 380CT CW2.py
@@ -22,15 +22,16 @@ class SSP():
         self.array = sorted( [ randint(0,max_n_bit_number) for i in range(length) ])#create a random sorted array
         self.target = randint(0,length*max_n_bit_number) #target is between 0 and the length*maxbitnumber
         self.length = len(self.array)
-        print ("Length of array: ",self.length, "Target: ", self.target,"\nArray: ",self.array)
+        print ("Length of array: ",self.length, "Target: ", self.target,"\nArray:\n",self.array)
+
+
      
     def exhaustive (self):
         start = timeit.default_timer()#start timer
 
-        if self.target > sum(self.array):
-            print ("Target is greater than sum of array")
+        if special_cases(self, start) == 0: #check for special cases
             return 0
-        
+
         for i in range(0, len(self.array)+1):
             for subset in itertools.combinations(self.array, i): #for each subset
                 if sum (subset) == self.target: #check if the subset sum is equal to the target
@@ -45,6 +46,9 @@ class SSP():
 
     def dynamic (self):
         start = timeit.default_timer() #start time
+
+        if special_cases(self, start) == 0: #check for special cases
+            return 0
         
         S = [[0 for x in range(self.length+1)] for y in range(self.target+1)]
         for i in range (self.length+1):
@@ -73,8 +77,39 @@ class SSP():
             print (stop - start) #total runtime
             print ("Target Not Found")
             return 0
+        
+def special_cases(self, start):
+        #if the target is greater than the sum of the array, it cannot be exactly found
+        if self.target > sum(self.array):
+            print ("Target is greater than sum of array")
+            stop = timeit.default_timer()
+            print (stop - start)
+            return 0
+        #the same goes for when the target is less than the smallest value in the array (but is not 0)
+        if self.target < self.array[0] and self.target != 0:
+            print ("Target is smaller than the smallest number in the array")
+            stop = timeit.default_timer()
+            print (stop - start)
+            return 0
+
+        #any values greater than the target should be ignored
+        print("\nValues greater than the target are being deleted from the array")
+        self.array = [x for x in self.array if x < self.target+1]
+        self.length = len(self.array)
+        print ("\nThe new array is:\n", self.array)
+        
+        #the target could be an element in the array already
+        if self.target in self.array:
+            print("\n \n Target is already in the array")
+            stop = timeit.default_timer()
+            print (stop - start)
+            return 0
+        
+
+
+                
 
 instance = SSP()
-instance.random_true_array(10,25)
-instance.dynamic()
+instance.random_array(10,25)
+instance.exhaustive()
 
