4513270918e019db772ac8fc60cbeeeadd1d1e8b
NickBeard117 <beardn@uni.coventry.ac.uk>
2017-01-05 19:49:09 +0000

    Updated file to fix minor errors + grasp

--- 380CT CW2.py
+++ 380CT CW2.py
@@ -2,6 +2,7 @@ import itertools
 import random
 from random import randint, sample
 import timeit
+import bisect
 
 class SSP():
 
@@ -22,7 +23,7 @@ class SSP():
         self.S = sorted( [ randint(0,max_n_bit_number) for i in range(length) ])#create a random sorted S
         self.target = randint(0,length*max_n_bit_number) #target is between 0 and the length*maxbitnumber
         self.length = len(self.S)
-        print ("Length of set: ",self.length, "Target: ", self.target,"\nSet:\n",self.S)
+        #print ("Length of set: ",self.length, "Target: ", self.target,"\nSet:\n",self.S)
 
     def random_reverse_set (self,bitlength, length):
         max_n_bit_number = 2**bitlength-1 #the max bit length is 2*bitlength-1
@@ -36,8 +37,8 @@ class SSP():
     def exhaustive (self):
         start = timeit.default_timer()#start timer
 
-        #if special_cases(self, start) == 0: #check for special cases
-           # return 0
+        #if instance.special_cases(start) == 0: #check for special cases
+            #return 0
 
         for i in range(0, len(self.S)+1):
             for subset in itertools.combinations(self.S, i): #for each subset
@@ -55,8 +56,8 @@ class SSP():
     def dynamic (self):
         start = timeit.default_timer() #start time
 
-        if special_cases(self, start) == 0: #check for special cases
-            return 0
+        #if instance.special_cases(start) == 0: #check for special cases
+            #return 0
         
         S = [[0 for x in range(self.length+1)] for y in range(self.target+1)]
         for i in range (self.length+1):
@@ -89,9 +90,11 @@ class SSP():
     def greedy (self):
         start = timeit.default_timer()#start timer
         
-        #if special_cases(self, start) == 0: #check for special cases
-           # return 0
+        #if instance.special_cases(start) == 0: #check for special cases
+           #return 0
+        
         total = 0
+        candidate = []
         used = []
   
         for i in range(0, len(self.S)):
@@ -101,13 +104,72 @@ class SSP():
                 
         stop = timeit.default_timer()
         
-        if self.target == 0:
-            print ("100,",stop - start)
-            return 1
-        #print ("This is the closest to the total using greedy: ", total)
+
+        #print ("\nThis is the closest to the total using greedy: ", total)
         #print ("Using these values: ", self.S)
         print ((total/self.target)*100 ,",",stop - start)
 
+
+    def grasp (self):
+        start = timeit.default_timer()#start timer
+        
+        #if instance.special_cases(start) == 0: #check for special cases
+            #return 0
+            
+        best = []
+        
+        for k in range (10000):
+            array = self.S[:]
+            j = 0
+            greedy = []
+            grasp = []
+            nd = []
+            nu = []
+
+            #this section creates the greedy array
+            while j < len(array):
+                candidate = random.choice(array)
+                if sum(greedy)<self.target:
+                    greedy.append(candidate)
+                    array.remove(candidate)
+                else:
+                    j += 1
+                
+            if sum(best) == 0:
+                best = greedy[:]
+
+            #local search here
+            if (random.randint(0, 1)) == 1:
+                rnu = random.choice(greedy)
+                nu = [x for x in array if x > rnu]
+                if len(nu) > 0:
+                    nu = nu[0]
+                    greedy.remove(rnu)
+                    greedy.append(nu)
+                    array.remove(nu)
+                    grasp = greedy[:]                
+            else:
+                rnd = random.choice(greedy)
+                nd = [x for x in array if x < rnd]
+                if len(nd) > 0:
+                    nd = nd[len(nd)-1]
+                    greedy.remove(rnd)
+                    greedy.append(nd)
+                    array.remove(nd)
+                    grasp = greedy[:]
+            #end of local search
+            
+            if abs(sum(grasp)-self.target) < abs(sum(best)-self.target):
+                print (best, sum(best))
+                best = grasp[:]
+           
+        #stop = timeit.default_timer()
+        #print (sum(best), self.target)
+        #print (sum(grasp))
+        if ((sum(best)/self.target)*100)>100:
+            print(200-((sum(best)/self.target)*100))
+        else:
+            print((sum(best)/self.target)*100)
         
     def special_cases(self, start):
             #if the target is greater than the sum of the set, it cannot be exactly found
@@ -116,6 +178,7 @@ class SSP():
                 stop = timeit.default_timer()
                 print (stop - start)
                 return 0
+            
             #the same goes for when the target is less than the smallest value in the ser (but is not 0)
             if self.target < self.S[0] and self.target != 0:
                 print ("Target is smaller than the smallest number in the set")
@@ -130,7 +193,7 @@ class SSP():
             print ("\nThe new S is:\n", self.S)
             
             #the target could be an element in the set already
-            if self.target in self.S:
+            if self.target in self.S or self.target == 0:
                 print("\n \n Target is already in the set")
                 stop = timeit.default_timer()
                 print (stop - start)
@@ -141,14 +204,14 @@ class SSP():
 
 
 instance = SSP()
-instance.random_reverse_set(10,20)
-instance.greedy()
+##instance.random_reverse_set(10,25)
+##instance.grasp()
 
-#for i in range(10, 30):
-#    print("n =", i)
-#    print ("\n\n")
-#    for _ in range(100):
-#        instance.random_set(10,i)
-#        instance.exhaustive()
+##for i in range(40, 41):
+##    print("n =", i)
+##    print ("\n\n")
+for _ in range(1):
+    instance.random_set(10,25)
+    instance.grasp()
        
 
